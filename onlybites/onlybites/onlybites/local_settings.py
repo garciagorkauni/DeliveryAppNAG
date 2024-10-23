b1729790d3eba09d6e51ac37d9540b2213067f87
Gorka Gacia <garcia.gorka@uni.eus>
2024-10-23 10:09:51 +0200

    fix: update project and app name

---
+++ onlybites/onlybites/onlybites/local_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'nag',
+        'USER': 'root',
+        'PASSWORD': 'Admin123',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
