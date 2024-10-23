68c7f5edae3832f6a20ccfedebe227e5234c0d25
Gorka Gacia <garcia.gorka@uni.eus>
2024-10-23 10:33:01 +0200

    feat(#7): Define Valoration model

--- onlybites/onlybites/onlybites_web/models.py
+++ onlybites/onlybites/onlybites_web/models.py
@@ -232,3 +232,10 @@ class Product(models.Model):
     calories = models.IntegerField()
     valoration_avg = models.FloatField()
     price = models.FloatField()
+
+class Valoration(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    value = models.IntegerField()
+    message = models.TextField()
