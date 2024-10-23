c4a52183292d02bbc95b215e52790db8d8d97e06
Gorka Gacia <garcia.gorka@uni.eus>
2024-10-23 10:25:49 +0200

    feat(#7): Define User model

--- onlybites/onlybites/onlybites_web/models.py
+++ onlybites/onlybites/onlybites_web/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=75)
+    surname = models.CharField(max_length=75)
+    birthdate = models.DateTimeField()
+    mail = models.CharField(max_length=75)
+    telephone = models.CharField(max_length=75)
+
+
