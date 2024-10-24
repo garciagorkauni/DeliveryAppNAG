43cb82a7b6dc0f1ccdb83302d6e4d9807eb30db8
Gorka Gacia <garcia.gorka@uni.eus>
2024-10-24 09:08:47 +0200

    feat(#8): Register modeloak in admin

--- onlybites/onlybites/onlybites_web/admin.py
+++ onlybites/onlybites/onlybites_web/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import User, Address, Product, Valoration, Cart, Image, Allergen, ProductAllergen
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Address)
+admin.site.register(Product)
+admin.site.register(Valoration)
+admin.site.register(Cart)
+admin.site.register(Image)
+admin.site.register(Allergen)
+admin.site.register(ProductAllergen)
