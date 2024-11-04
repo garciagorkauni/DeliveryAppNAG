3fbf7bfd985f3dba2a88aec6ad2188d71ed285a6
Gorka Gacia <114485898+garciagorkauni@users.noreply.github.com>
2024-11-04 11:40:29 +0100

    feat(#36): Cart (#48)
    * Update local_settings.py
    
    * feat(#36): Define Cart and Order models
    
    * feat(#36): Register order model in admin
    
    * feat(#36): Develop add to cart option
    
    * feat(#36): Develop quit option
    
    * feat(#36): Start developing change quantity options
    
    * feat(#36): Develop change quantity options
    
    * feat(#36): Quit addresses from cart
    
    * feat(#36): Develop payment simulation
    
    * fix: Add address to order model
    
    ---------
    
    Co-authored-by: Gorka Garcia <144927018+gorka-cs@users.noreply.github.com>

--- onlybites/onlybites/onlybites_web/forms.py
+++ onlybites/onlybites/onlybites_web/forms.py
@@ -11,3 +11,14 @@ class AddressForm(forms.ModelForm):
     class Meta:
         model=Address
         fields=['name','surname', 'telephone', 'country', 'postal_code', 'city', 'address1', 'address2']
+
+class PaymentForm(forms.Form):
+    address = forms.ChoiceField(label="Select Address", choices=[])
+
+    def __init__(self, *args, profile=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        if profile:
+            self.fields['address'].choices = [
+                (address.address_id, f"{address.address1}, {address.city}, {address.country}")
+                for address in Address.objects.filter(profile=profile)
+            ]
--- onlybites/onlybites/onlybites_web/urls.py
+++ onlybites/onlybites/onlybites_web/urls.py
@@ -13,7 +13,11 @@ urlpatterns = [
 
     # cart
     path('cart/', views.cart, name='cart'),
-
+    path('add-cart/<int:product_id>/', views.add_cart, name='add_cart'),
+    path('delete-cart/<int:cart_id>/', views.delete_cart, name='delete_cart'),
+    path('reduce-cart/<int:product_id>/', views.reduce_cart, name='reduce_cart'),
+    path('payment/', views.payment, name='payment'),
+    
     # profile
     path('profile/', views.profile, name='profile'),
     path('add-address/', views.add_address, name='add_address'),
