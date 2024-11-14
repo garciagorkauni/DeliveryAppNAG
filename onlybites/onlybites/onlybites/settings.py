a617392df5f45c67ccc447e689554af4b290d116
Gorka Gacia <garcia.gorka@uni.eus>
2024-11-14 08:28:47 +0100

    feat(#70): Implement image field for product

--- onlybites/onlybites/onlybites/settings.py
+++ onlybites/onlybites/onlybites/settings.py
@@ -188,6 +188,9 @@ import os
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 LOCALE_PATHS = [
     os.path.join(BASE_DIR, 'onlybites', 'locale'),
 ]
